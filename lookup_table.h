ac19d7dc5a96151908b60aafdb2cec1d8f125a19
Gleb Zhelezov <gleb.zh@gmail.com>
2021-02-26 13:57:08 -0700

    updated headers

--- lookup_table.h
+++ lookup_table.h
@@ -2,4 +2,5 @@
 #undef INTERFACE
 int compressed_rep(int a,int b,int *two2three);
 void calculate_two2three(int **two2three,int n);
+void fill_two2three(int *two2three,int n);
 int ipow(int a,int b);
--- scores.h
+++ scores.h
@@ -1,7 +1,14 @@
 /* This file was automatically generated.  Do not edit! */
 #undef INTERFACE
-int compressed_rep(int a,int b,int *two2three);
+int ipow(int a,int b);
 void calculate_two2three(int **two2three,int n);
+int compressed_rep(int a,int b,int *two2three);
+void fill_compressed_score_representation(int *left_sets,int *right_sets,int *weights,int n_biparts,int n_species,int *scores,int *two2three);
+void fill_compressed_score_representation(int *left_sets,int *right_sets,int *weights,int n_biparts,int n_species,int *scores,int *two2three);
+int n_common_triplets(int a,int b,int c,int d);
 int n_common_triplets(int a,int b,int c,int d);
 int combinations_2(int n);
+int combinations_2(int n);
 void get_compressed_score_representation(int *left_sets,int *right_sets,int *weights,int n_biparts,int n_species,int **scores);
+void get_compressed_score_representation(int *left_sets,int *right_sets,int *weights,int n_biparts,int n_species,int **scores);
+int main_test();
