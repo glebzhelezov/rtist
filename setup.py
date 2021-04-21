e014d44801b44b9a82c239c8badafba0c2eb123b
Gleb Zhelezov <gleb.zh@gmail.com>
2021-04-20 19:09:56 -0600

    Reformatted setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,13 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 
 comb2_extension = Extension(
-        name='comb2',
-        sources=['triplet_omp_py.pyx'],
-        libraries=['ctriplet'],
-        library_dirs=['lib'],
-        include_dirs=['lib'],
-        extra_compile_args=['-fopenmp'],
-        extra_link_args=['-fopenmp'],
-        )
+    name="comb2",
+    sources=["triplet_omp_py.pyx"],
+    libraries=["ctriplet"],
+    library_dirs=["lib"],
+    include_dirs=["lib"],
+    extra_compile_args=["-fopenmp"],
+    extra_link_args=["-fopenmp"],
+)
 
-setup(
-        name='comb2',
-        ext_modules=cythonize([comb2_extension], language_level=3)
-        )
+setup(name="comb2", ext_modules=cythonize([comb2_extension], language_level=3))
