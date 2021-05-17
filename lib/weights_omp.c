0b12f627e4d1e85ea51502b7125608db3e4f40ab
Gleb Zhelezov <gleb.zh@gmail.com>
2021-05-17 08:52:38 -0600

    removed all buffers and private copies of array and replaced with omp atomic

--- lib/weights_omp.c
+++ lib/weights_omp.c
@@ -140,16 +140,12 @@ void fill_compressed_weight_representation(
     int loop_progress = 0;
     /* We'll make the buffer sizes slightly different to avoid
      * unnecessary locks. Set the seed to 0 for reproducibility. */
-    srand(0);
     progressbar *progbar = progressbar_new("Progress", n_subsets);
 #pragma omp parallel num_threads(n_threads)
     {
-        int private_bufsize = (int) ((0.7 + 0.3*(rand()%200)/100)*bufsize);
+        //int private_bufsize = (int) ((0.7 + 0.3*(rand()%200)/100)*bufsize);
         /* int private_bufsize = int(0.7*bufsize) + (0.3*bufsize(rand() % 200)/100.0);*/
         /* int *weights_private = calloc(2*ipow(3,n_species-1), sizeof(int)); */
-        int *weights_buffer = calloc(private_bufsize, sizeof(int));
-        int *positions_buffer = calloc(private_bufsize, sizeof(int));
-        int buffer_position=0;
         int counter_private = 0;
         int n_threads_assigned = omp_get_num_threads();
         int output_step = round(n_subsets/(100*n_threads_assigned));
@@ -205,27 +201,8 @@ void fill_compressed_weight_representation(
                                 //weights_private[rep] += n_triplets * bipart_weight;
                                 /*weights_private[rep] += weight_increment;*/
                                 /* We'll add this to the global weights array later */
-                                weights_buffer[buffer_position] += weight_increment;
-                                positions_buffer[buffer_position] = rep;
-                                buffer_position++;
-
-                                /* Check if it's time to update the weights
-                                 * array */
-                                if (buffer_position == private_bufsize) {
-                                    /* Only one thread accesses array to update */
-# pragma omp critical
-                                    for (int i=0; i<private_bufsize; i++) {
-                                        weights[positions_buffer[i]] += 
-                                            weights_buffer[i];
-                                    }
-                                    /* reset everything */
-                                    buffer_position = 0;
-                                    memset(weights_buffer, 0, private_bufsize*sizeof(int));
-                                    /* For large-ish arrays it is faster to calloc */
-                                    /*free(weights_buffer);
-                                    weights_buffer = calloc(private_bufsize, sizeof(int));*/
-
-                                }
+# pragma omp atomic update
+                                weights[rep] += weight_increment;
 
                                 /* This is necessary to break out of an endless
                                  * loop! */
@@ -256,19 +233,7 @@ void fill_compressed_weight_representation(
                 }
             }
         }
-            /* Need to add in updates from partially-full buffers */
-# pragma omp critical
-        {
-            for (int i=0; i<buffer_position; i++) {
-                weights[positions_buffer[i]] += weights_buffer[i];
-            }
-            /*for (int i=0; i<2*ipow(3,n_species-1); i++) {
-                weights[i] += weights_private[i];
-            }*/
-        }
         /*free(weights_private);*/
-        free(weights_buffer);
-        free(positions_buffer);
     }
     progressbar_update(progbar, n_subsets);
     progressbar_finish(progbar);
